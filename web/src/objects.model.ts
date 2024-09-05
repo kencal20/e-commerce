92d9ee4221ba1fdb7e2ac18fd581f9e9e1eecaf9
kencal20 <kenadjei20@gmail.com>
2024-09-05 11:14:32 +0000

    object types module

---
+++ web/src/objects.model.ts
@@ -0,0 +1,54 @@
+interface cardProps {
+  title: string;
+  children?: React.ReactNode
+  className?: string
+}
+interface productsProps {
+  name: string
+  price: number;
+  brand: string
+  img: string
+}
+
+interface slidesProps {
+  id: string;
+  src: string;
+  alt: string;
+}
+
+interface inputProps {
+  name: string
+  label: string
+  value: string
+  type: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  required: boolean
+  className: string
+}
+
+interface userProps {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  phone: string;
+}
+
+interface statusProps {
+  text: string,
+  variant: 'danger' | 'success' | undefined
+}
+
+interface protectedRouteProps {
+  isAuthenticated: boolean;
+  children: JSX.Element;
+};
+export interface componentProps {
+  cardProps: cardProps;
+  productsProps: productsProps;
+  slidesProps: slidesProps;
+  inputProps: inputProps
+  userProps: userProps
+  statusProps: statusProps;
+  protectedRouteProps: protectedRouteProps;
+}
