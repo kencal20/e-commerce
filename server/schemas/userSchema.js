22547476d11439cbb020245e452320ae41068906
kencal20 <kenadjei20@gmail.com>
2024-09-05 11:12:24 +0000

    controllers schemas

---
+++ server/schemas/userSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'customer', 'item_manager'],
+    default: 'customer'
+  }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
