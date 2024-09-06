248b9aba426ec0d315cd83edca0d42c50ca5e43b
kencal20 <kenadjei20@gmail.com>
2024-09-06 11:11:07 +0000

    update userCreation component font style

--- web/src/screens/createUser.tsx
+++ web/src/screens/createUser.tsx
@@ -158,7 +158,7 @@ export default function CreateUser({ }: Props) {
                             <input id="default-checkbox" type="checkbox" value=""
                                 onClick={togglePasswordVisibility}
                                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password</label>
+                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-slate-800">Show Password</label>
                         </div>
 
                     <SubmitButtonComponent isloading={isloading} />
