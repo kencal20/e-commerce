bf7a94f2cc61c7a2d95504782ef6a41ed2870fc4
kencal20 <kenadjei20@gmail.com>
2024-09-08 21:17:13 +0000

    adding  colors variables to tailwindConfig

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,9 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark_theme-background': '#1a202c',
+        'dark_theme-text': '#a0aec0',       
+        'dark_theme-link': '#63b3ed',     
+        'dark_theme-header': '#ffff', 
+        "dark_theme-card":'#1f2937'    
+  
+      },
+    },
   },
-  plugins: [
-
-  ],
+  plugins: [],
 }
