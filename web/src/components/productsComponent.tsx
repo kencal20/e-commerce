8a27ddbc9102d85dd801a3e004984b5b7d620cf3
kencal20 <kenadjei20@gmail.com>
2024-09-08 21:20:13 +0000

    calling appointed color variables in the individual components

--- web/src/components/productsComponent.tsx
+++ web/src/components/productsComponent.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { products, } from "../data/productsData"
+import { products } from "../data/productsData"
 import CardComponent from "./cardComponent"
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
 import { componentProps } from "../objects.model"
@@ -13,33 +13,33 @@ type Props = {
 
 export default function ProductsComponent({ showShopLink }: Props) {
     return (
-        <div className="px-10 py-10 font-medium  sm:flex-wrap ">
-            <div className="flex flex-row justify-between p-4">
+        <div className="px-10 py-10 font-medium sm:flex-wrap bg-white text-black dark:bg-dark_theme-background dark:text-dark_theme-text">
+            <div className="flex flex-row justify-between p-4  dark:text-dark_theme-header">
                 <h1 className="text-4xl">PRODUCTS</h1>
-                {showShopLink && (<u>
-                    <Link to="/products">GO TO SHOP</Link>
-                </u>)
-                }
+                {showShopLink && (
+                    <u>
+                        <Link to="/products" className="text-blue-500 dark:text-dark_theme-link">
+                            GO TO SHOP
+                        </Link>
+                    </u>
+                )}
             </div>
-            <div className="flex flex-wrap justify-between ">
-                {
-                    products.map((product, index) => (
-                        <CardComponent
-                            title={product.name}
-                            key={index}
-                            className="text-2xl font-medium"
-                            childrenClassName=""
-                            titleClassName=""
-                        >
-                            <img src={product.img} alt="" />
-                            <div className="flex justify-between mt-5">
-                                <p>${product.price}</p>
-                                <FontAwesomeIcon icon={faShoppingCart} />
-                            </div>
-                        </CardComponent>
-                    ))
-                }
-
+            <div className="flex flex-wrap justify-between">
+                {products.map((product, index) => (
+                    <CardComponent
+                        title={product.name}
+                        key={index}
+                        className="text-2xl font-medium shadow-lg bg-slate-100 dark:bg-dark_theme-background border-0"
+                        childrenClassName=""
+                        titleClassName=""
+                    >
+                        <img src={product.img} alt={product.name} />
+                        <div className="flex justify-between mt-5">
+                            <p>${product.price}</p>
+                            <FontAwesomeIcon icon={faShoppingCart} />
+                        </div>
+                    </CardComponent>
+                ))}
             </div>
         </div>
     )
--- web/src/components/servicesComponent.tsx
+++ web/src/components/servicesComponent.tsx
@@ -3,23 +3,23 @@ import services from '../data/servicesData';
 
 export default function ServicesComponent() {
     return (
-        <div className="bg-white dark:bg-gray-900 py-16">
+        <div className="bg-white dark:bg-dark_theme-background py-16">
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-4xl font-bold text-gray-900 dark:text-white text-center mb-12">Our Services</h1>
+                <h1 className="text-4xl font-bold text-gray-900 dark:text-dark_theme-header text-center mb-12">Our Services</h1>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {services.map((service, index) => (
                         <CardComponent
                             key={index}
                             title={service.title}
-                            className="bg-gray-50 dark:bg-gray-800 rounded-lg  border-0 shadow-lg"
+                            className="bg-gray-50 dark:bg-dark_theme-card rounded-lg  border-0 shadow-lg"
                             childrenClassName=''
                             titleClassName="dark:text-gray-400 "
                         >
                             <div className="mb-4">
                                 {service.icon}
                             </div>
-                            <p className="text-gray-600 dark:text-gray-400">{service.description}</p>
+                            <p className="text-gray-600 dark:text-dark_theme-text">{service.description}</p>
                         </CardComponent>
                     ))}
                 </div>
